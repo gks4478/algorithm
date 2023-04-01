752cfb9a32ff5147dd9238a56056b4dbe51563f4
gks4478 <77867734+gks4478@users.noreply.github.com>
2023-04-01 10:25:11 +0900

    Create 레벨0_세균증식_(16_100).py
    레벨0_세균증식_16/100

---
+++ Programmers/레벨0_세균증식_(16_100).py
@@ -0,0 +1,8 @@
+# 문제 주소
+# https://school.programmers.co.kr/learn/courses/30/lessons/120910
+def solution(n, t):
+    answer = n
+    
+    for i in range(t) :
+        answer *= 2
+    return answer
