9ac25ef557072733c6c67e06417c0d43bf105e0c
gks4478 <77867734+gks4478@users.noreply.github.com>
2023-06-14 23:44:57 +0900

    Create 레벨0_배열 원소의 길이_(56_100).py
    레벨0 배열 원소의 길이 56/100

---
+++ Programmers/레벨0_배열 원소의 길이_(56_100).py
@@ -0,0 +1,7 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/120854
+
+def solution(strlist):
+    answer = []
+    for i in range(len(strlist)):
+        answer.append(len(strlist[i]))
+    return answer
